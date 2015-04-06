0bdec42edf01490cc408e1ffbb40609aa9e24ddc
Carlos Perez Lopez <carlospzlz@gmail.com>
2015-04-06 19:58:58 +0100

    Advanced with tinderbot.
    TODO:
    say hello to matches with no message.
    massive like.

--- src/tinderbot.py
+++ src/tinderbot.py
@@ -45,33 +45,21 @@ class TinderBotException( Exception ):
 class TinderBot( object ):
 	def __init__( self ):
 		self.__headers = HEADERS
-		self.__userId = 0
+		self.__profile = {}
 		self.__storePath = ""
-		self.__recommendations = {}
-		self.__matches = {}
-		self.__blocks = {}
+		self.__people = {}
+		self.__matches = []
+		self.__matchedPeople = {}
 		self.__remainingLikes = sys.maxsize
 
-	def __loadRecommendations( self ):
-		recommendationDirs = os.listdir( self.__storePath )
-		for recommendationDir in recommendationDirs:
-			profileFile = "{0}/{1}/profile.json".format( self.__storePath,
-				recommendationDir )
-			if not os.path.exists( profileFile ):
-				continue
-			with open(profileFile, "r") as inFile:
-				recommendation = json.load( inFile )
-				self.__recommendations[recommendation["_id"]] = recommendation
-				msg = "{0}'s profile loaded.".format( recommendation["name"] )
-				self.__printMsg( msg )
-		self.__printMsg( "{0} recommendations loaded.".format(
-			len( self.__recommendations ) ) )
+	def getPeople( self ):
+		return self.__people
 
-	def getRecommendations( self ):
-		return self.__recommendations
-
-	def getMatched( self ):
+	def getMatches( self ):
 		return self.__matches
+
+	def getMatchedPeople( self ):
+		return self.__matchedPeople
 
 	def __printMsg( self, msg ):
 		time = datetime.datetime.now().strftime( TIME_FORMAT )
@@ -85,6 +73,36 @@ class TinderBot( object ):
 			return False
 		return True
 
+	def __requestProfile( self ):
+		self.__printMsg( "Requesting your profile ..." )
+		response = requests.get( "{0}/profile".format( HOST ),
+			headers=self.__headers )
+		if not self.__validResponse( response ):
+			return
+		self.__profile = response.json()
+		self.__printMsg( "Your profile was loaded." )
+		self.__storePath = "{0}/{1}_{2}_store".format( STORE_BASE_PATH,
+			self.__profile["name"], self.__profile["_id"] )
+		self.__printMsg( "Store path set." )
+
+	def __loadPeople( self ):
+		if not os.path.isdir( self.__storePath ):
+			self.__printMsg( "Cannot load people: Store doesn't exist" )
+			return
+		peopleDirs = os.listdir( self.__storePath )
+		for personDir in peopleDirs:
+			profileFile = "{0}/{1}/profile.json".format( self.__storePath,
+				personDir )
+			if not os.path.exists( profileFile ):
+				continue
+			with open(profileFile, "r") as inFile:
+				person = json.load( inFile )
+			self.__people[person["_id"]] = person
+			msg = "{0}'s profile loaded.".format( person["name"] )
+			self.__printMsg( msg )
+			msg = "{0} people loaded.".format( len( self.__people ) )
+			self.__printMsg( msg )
+
 	def authenticate( self, token, id_ ):
 		data = json.dumps( {"facebook_token": token, "facebook_id": id_} )
 		self.__printMsg( "Authenticating ..." )
@@ -95,85 +113,87 @@ class TinderBot( object ):
 			return
 		self.__headers["X-Auth-Token"] = response.json()["token"]
 		self.__userId = response.json()["user"]["_id"]
-		self.__storePath = "{0}/{1}".format( STORE_BASE_PATH, self.__userId )
 		self.__printMsg( "Athentication succesfully." )
-		self.__loadRecommendations()
+		self.__requestProfile()
+		self.__loadPeople()
 
-	def __saveProfile( self, recommendation, profileDir ):
+	def __saveProfile( self, person, profileDir ):
 		profileDestination = "{0}/profile.json".format( profileDir )
-		self.__printMsg( "Saving {0}'s profile ...".format( recommendation["name"] ) )
+		self.__printMsg( "Saving {0}'s profile ...".format( person["name"] ) )
 		with open(profileDestination, "w") as outFile:
-			json.dump( recommendation, outFile )
+			json.dump( person, outFile )
 	
-	def __savePhotos( self, recommendation, photosDir ):
-		self.__printMsg( "Saving {0}'s photos ...".format( recommendation["name"] ) )
-		for photo in recommendation["photos"]:
+	def __savePhotos( self, person, photosDir ):
+		self.__printMsg( "Saving {0}'s photos ...".format( person["name"] ) )
+		for photo in person["photos"]:
 			url = photo["url"]
 			photoDestination = "{0}/{1}".format( photosDir, photo["fileName"] )
 			urllib.request.urlretrieve( url, photoDestination )
 
-	def __saveIndex( self, recommendation, indexDir, photosDir ):
-		self.__printMsg( "Indexing {0} ...".format( recommendation["name"] ) )
-		if not recommendation["photos"]:
-			self.__printMsg( "Cannot save index for {0}: No pictures.".format(
-				recommendation["name"] ) )
+	def __indexPerson( self, person, indexDir ):
+		self.__printMsg( "Indexing {0} in {1} ...".format(
+			person["name"], indexDir.rsplit("/",1)[1] ) )
+		if not person["photos"]:
+			self.__printMsg( "Cannot index {0}: No pictures.".format(
+				person["name"] ) )
 			return
-		for photo in recommendation["photos"][::-1]:
+		for photo in person["photos"][::-1]:
 			# if no valid main the first photo is used
 			if photo.get("main"):
 				# it may not have the key main
 				break
-		photoDestination = "{0}/{1}".format( photosDir, photo["fileName"] )
+		photoDestination = "{0}/{1}_{2}/photos/{3}".format(
+			self.__storePath, person["name"], person["_id"], photo["fileName"] )
 		_, extension = os.path.splitext( photo["fileName"] )
-		indexLink = "{0}/{1}_{2}{3}".format( indexDir, recommendation["name"],
-			recommendation["_id"], extension )
+		indexLink = "{0}/{1}_{2}{3}".format( indexDir, person["name"],
+			person["_id"], extension )
 		if os.path.exists( indexLink ):
 			os.remove( indexLink )
 		os.symlink( photoDestination, indexLink )
 
-	def __saveRecommendation( self, recommendation ):
+	def __savePerson( self, person ):
 		# Profile
-		profileDir = "{0}/{1}_{2}".format( self.__storePath, recommendation["name"],
-			recommendation["_id"] )
+		profileDir = "{0}/{1}_{2}".format( self.__storePath, person["name"],
+			person["_id"] )
 		if not os.path.isdir( profileDir ):
 			os.makedirs( profileDir )
-		self.__saveProfile( recommendation, profileDir )
+		self.__saveProfile( person, profileDir )
 		# Photos
-		photosDir = "{0}/{1}_{2}/photos".format( self.__storePath, recommendation["name"],
-			recommendation["_id"] )
+		photosDir = "{0}/{1}_{2}/photos".format( self.__storePath, person["name"],
+			person["_id"] )
 		if not os.path.isdir( photosDir ):
 			os.makedirs( photosDir )
-		self.__savePhotos( recommendation, photosDir )
+		self.__savePhotos( person, photosDir )
 		# Index
 		indexDir = "{0}/index".format( self.__storePath )
 		if not os.path.isdir( indexDir ):
 			os.makedirs( indexDir )
-		self.__saveIndex( recommendation, indexDir, photosDir )
+		self.__indexPerson( person, indexDir )
 	
-	def __getPingTime( self, recommendation ):
-		pingTimeString = recommendation["ping_time"].split( ".", 1)[0]
+	def __getPingTime( self, person ):
+		pingTimeString = person["ping_time"].split( ".", 1)[0]
 		pingTime = datetime.datetime.strptime( pingTimeString,
 			"%Y-%m-%dT%H:%M:%S" )
 		return pingTime
 
-	def __updateRecommendation( self, recommendation ):
-		id_ = recommendation["_id"]
-		name = recommendation["name"]
-		if id_ in self.__recommendations:
-			savedPingTime = self.__getPingTime( self.__recommendations[id_] )
-			newPingTime = self.__getPingTime( recommendation )
+	def __updatePerson( self, person ):
+		id_ = person["_id"]
+		name = person["name"]
+		if id_ in self.__people:
+			savedPingTime = self.__getPingTime( self.__people[id_] )
+			newPingTime = self.__getPingTime( person )
 			if newPingTime > savedPingTime:
 				self.__printMsg( "Updating {0} in the store:".format( name ) )
-				self.__saveRecommendation( recommendation )
-				self.__recommendations[id_] = recommendation
+				self.__savePerson( person )
+				self.__people[id_] = person
 				self.__printMsg( "{0} updated.".format( name ) )
 			else:
 				msg = "{0} is up to date in the store.".format( name )
 				self.__printMsg( msg )
 		else:
 			self.__printMsg( "Adding {0} to the store:".format( name ) )
-			self.__saveRecommendation( recommendation )
-			self.__recommendations[id_] = recommendation
+			self.__savePerson( person )
+			self.__people[id_] = person
 			self.__printMsg( "{0} added.".format( name ) )
 				
 	def requestRecommendations( self ):
@@ -185,10 +205,21 @@ class TinderBot( object ):
 		recommendations = response.json()["results"]
 		self.__printMsg( "{0} recommendations:".format(
 			len( recommendations ) ) )
-		for recommendation in recommendations:
-			self.__updateRecommendation( recommendation )
-		self.__printMsg( "{0} total  recommendations.".format(
-			len( self.__recommendations ) ) )
+		for person in recommendations:
+			self.__updatePerson( person )
+		self.__printMsg( "{0} total people.".format(
+			len( self.__people ) ) )
+
+	def updateStore( self ):
+		if not os.path.isdir( self.__storePath ):
+			self.__printMsg( "Cannot update store: Store doesn't exist" )
+			return
+		for id_, person in self.__people.items():
+			self.__updatePerson( person )
+			
+	def __updateMatch( self, match ):
+		person = match["person"]
+		self.__updateRecommendation( person )
 
 	def requestUpdates( self ):
 		data = json.dumps( {"last_activity_date": ""} )
@@ -198,22 +229,35 @@ class TinderBot( object ):
 			return
 		responseDict = response.json()
 		self.__matches = responseDict["matches"]
-		self.__blocks = reponseDict["blocks"]
+		self.__printMsg( "Updating matches ..." )
+		matchDir = "{0}/matches".format( self.__storePath )
+		if not os.path.isdir( matchDir ):
+			os.makedirs( matchDir )
+		for match in self.__matches:
+			person = match["person"]
+			self.__printMsg( "Updating match with {0} ...".format(
+				person["name"] ) )
+			self.__updatePerson( person )
+			self.__matchedPeople[person["_id"]] = person
+			self.__indexPerson( person, matchDir )
+			self.__printMsg( "{0}'s match updated.".format(
+				person["name"] ) )
+		self.__blocks = responseDict["blocks"]
 
 	def like( self, id_ ):
-		if id_ not in self.__recommendations:
+		if id_ not in self.__people:
 			self.__printMsg( "Don't know about her/him" )
 			return
-		recommendation = self.__recommendations[id_]
-		self.__printMsg( "Liking {0} ...".format( recommendation["name"] ) )
+		person = self.__people[id_]
+		self.__printMsg( "Liking {0} ...".format( person["name"] ) )
 		requestMsg = "{0}/like/{1}".format( HOST, id_ )
 		response = requests.get( requestMsg, headers=self.__headers )
 		if not self.__validResponse( response ):
 			return
 		responseDict = response.json()
 		if responseDict["match"]:
-			self.__printMsg( "You've got a MATCH with {0}!".format(
-				recommendation["name"] ) )
+			msg = "You've got a MATCH with {0}!".format( person["name"] ) )
+			self.__printMsg( msg )
 		self.__remainingLikes = responseDict["likes_remaining"]
 		self.__printMsg( "{0} likes remaining.".format(
 			self.__remainingLikes ) )
